c07e402d606848ea5f8d0db302af036811e476a9
Salmaahmed18 <ramadansalma23@gmail.com>
2024-05-04 00:54:00 +0300

    made the user profile page

--- Web project/profile.js
+++ Web project/profile.js
@@ -1,6 +1,10 @@
 document.addEventListener('DOMContentLoaded', function() {
     var username = localStorage.getItem('username');
-    var userInfo = document.getElementById('userInfo');
+    var userInfoForm = document.getElementById('userInfoForm');
+    var nameField = document.getElementById('name');
+    var emailField = document.getElementById('email');
+    var phoneField = document.getElementById('phone');
+    var passwordField = document.getElementById('password');
     var logoutButton = document.getElementById('logoutButton');
 
     if (username) {
@@ -10,23 +14,22 @@ document.addEventListener('DOMContentLoaded', function() {
         });
 
         if (user) {
-            userInfo.textContent = 'Name: ' + user.name ;
-            userInfo.innerHTML += '<br>Email: ' + user.email;
-            userInfo.innerHTML += '<br>Phone: ' + user.phone;
-            userInfo.innerHTML += '<br>password: ' + user.password;
+            nameField.value = user.name;
+            emailField.value = user.email;
+            phoneField.value = user.phone;
+            
         } else {
+            userInfoForm.style.display = 'none'; // Hide form if user not found
             userInfo.textContent = 'User not found.';
-            logoutButton.style.display = 'none'; // Hide logout button if user not found
         }
     } else {
+        userInfoForm.style.display = 'none'; // Hide form if not logged in
         userInfo.textContent = 'Not logged in.';
-        logoutButton.style.display = 'none'; // Hide logout button if not logged in
     }
 
     logoutButton.addEventListener('click', function(event) {
         localStorage.setItem('mode', '0'); // Reset user mode
         localStorage.removeItem('username'); // Clear username
         history.back();
-
     });
 });
